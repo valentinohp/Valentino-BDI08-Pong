b54477f6c2104b5dc0424e53e561ae36d0dc72f3
Valentino Herdyan Permadi <valentino.herdyan@ui.ac.id>
2022-08-02 23:06:34 +0700

    Features addition - Problem Solved

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -5,26 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void PlayGame() 
     { 
         SceneManager.LoadScene("Game"); 
         Debug.Log("Created by Valentino Herdyan Permadi - BDI08");
     }
 
-    public void OpenAuthor() 
-    { 
+    public void OpenCredit() 
+    {
+        SceneManager.LoadScene("Credit");
         Debug.Log("Created by Valentino Herdyan Permadi - BDI08");
     } 
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
 }
---
+++ Assets/Scripts/URLHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLHandler : MonoBehaviour
+{
+    public void OpenURL()
+    {
+        Application.OpenURL("https://valentinohp.itch.io/");
+    }
+}
